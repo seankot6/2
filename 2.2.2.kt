ec8335e371111610032fd03f9c42730159be547a
seany <shhekwu@gmail.com>
2024-09-22 16:16:07 +0900

    Add files via upload

---
+++ 2.2.2.kt
@@ -0,0 +1,10 @@
+// Задание 2
+fun main() {
+    val x2 = false
+    val y2 = true
+    val z2 = false
+
+    println("x2 || z2: ${x2 || z2}") // false
+    println("x2 && y2: ${x2 && y2}") // false
+    println("x2 && z2: ${x2 && z2}") // false
+}
